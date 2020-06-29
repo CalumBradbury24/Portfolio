05ed351ef1e7903c52d3a00bd1521bf3e54227d0
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-06-29 08:25:54 +0100

    moved WordCloud into about me section and improved mobile responsiveness

--- src/Components/About-me/About-me.jsx
+++ src/Components/About-me/About-me.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./About-me.styles.scss";
 import CustomButton from "../../Components/CustomButton/CustomButton";
 import Description from './description';
+import WordCloud from '../WordCloud/WordCloud';
 
 const AboutMe = () => {
   return (
     <div className="About-me animated fadeInLeft delay-1s">
       <h2>About Me</h2>
       <Description />
+      <WordCloud />
       <CustomButton />
     </div>
   );
