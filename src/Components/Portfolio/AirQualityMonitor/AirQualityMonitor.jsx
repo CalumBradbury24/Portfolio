04df3413462e462f7b7a7675e6989f35720db9ad
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-07-03 10:26:52 +0100

    added air-quality-application

--- src/Components/Portfolio/AirQualityMonitor/AirQualityMonitor.jsx
+++ src/Components/Portfolio/AirQualityMonitor/AirQualityMonitor.jsx
@@ -1,42 +1,45 @@
 import React from "react";
 import "./AirQualityMonitor.styles.scss";
-//import clothesPic from "../../../assets/phoenixClothing.PNG";
+import air from "../../../assets/airQual.jpg";
 import { ReactComponent as Logo } from "../../../assets/github.svg";
 
 const AirQualityMonitor = () => {
   return (
-    <div className="AirQual ">
-      <img className = "air-image" alt="Air Monitor Application" />
-      <div>
+    <div className="AirQuality ">
+      <img className="air-image" src={air} alt="Air Monitor Application" />
+      <div className ='air-container'>
         <h2>Air Quality Monitor</h2>
-        <p>
+        <p className='par'>
           An Air Quality Monitor web application built as a hobby. This project
           uses a Raspberry PI and an assortment of sensors to monitor
-          particulate matter in the atmosphere and displays the readings in a
-          web application.
+          particulate matter in the atmosphere and displays the readings on a
+          dashboard.
         </p>
-        <h3>Project Stack:</h3>
-        <span className="stack">
-          <p className="stack-item">HTML5</p>
-          <p className="stack-item">CSS</p>
-          <p className="stack-item">React.js</p>
-          <p className="stack-item">Node.js</p>
-          <p className="stack-item">MongoDB</p>
-          <p className="stack-item">Express.js</p>
-          <p className="stack-item">PMS5003 Python Library</p>
+        <h4 className ='airQuality-stack'>Project Stack:</h4>
+        <span className="air-stack">
+          <p className="stack-item par"><i>HTML5</i></p>
+          <p className="stack-item par"><i>CSS</i></p>
+          <p className="stack-item par"><i>React.js</i></p>
+          <p className="stack-item par"><i>Node.js</i></p>
+          <p className="stack-item par"><i>MongoDB</i></p>
+          <p className="stack-item par"><i>Express.js</i></p>
+          <p className="stack-item par"><i>PMS5003 Python Library</i></p>
         </span>
-        <h3>Hardware:</h3>
-        <span className="stack">
-          <p className="stack-item"><i>Raspberry Pi 3 Model B+</i></p>
-          <p className="stack-item"><i>PMS5003 Particulate Matter Sensor</i></p>
+        <h4 className ='airQuality-stack'>Hardware:</h4>
+        <span className="air-stack">
+          <p className="stack-item par">
+            <i>Raspberry Pi 3 Model B+</i>
+          </p>
+          <p className="stack-item par">
+            <i>PMS5003 Particulate Matter Sensor</i>
+          </p>
         </span>
-        <h3>This project can be found at:</h3>
         <p className="stack-item">
           <a href="https://air-qual-monitor.herokuapp.com/">
             https://air-qual-monitor.herokuapp.com/
           </a>
         </p>
-        <span className="git-image">
+        <span className="ima">
           <a href="https://github.com/CalumBradbury24/AirQualityMonitor">
             <Logo />
           </a>
