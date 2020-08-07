f05cbc1eb2e7b94900b1aa164d4fc5e3c7144db9
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-08-07 13:24:02 +0100

    added responsive navbar for mobile

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NavBar.styles.scss";
 import { Link, useLocation } from "react-router-dom";
-
+import logo from "../../assets/hamburger.svg";
 const NavBar = () => {
   let location = useLocation(); //React Router hook to get current path
-
+  const [navOpen, setNavOpen] = useState(false);
   return (
-    <div className="nav sticky">
-      <ul className="list">
+    <nav className="responsive-toolbar">
+      <ul className={navOpen ? "active" : ""}>
+      <figure onClick={() => setNavOpen(!navOpen)}>
+          <img src={logo} height="40px" width="40px" alt="logo"></img>
+        </figure>
         <Link
           className={
-            "navbar-item " +
+            "list-item " +
             (location.pathname === "/" ? "stayOrange" : "default")
           }
           to="/"
@@ -19,7 +22,7 @@ const NavBar = () => {
         </Link>
         <Link
           className={
-            "navbar-item " +
+            "list-item " +
             (location.pathname === "/resume" ? "stayOrange" : "default")
           }
           to="/resume"
@@ -28,7 +31,7 @@ const NavBar = () => {
         </Link>
         <Link
           className={
-            "navbar-item " +
+            "list-item " +
             (location.pathname === "/projects" ? "stayOrange" : "default")
           }
           to="projects"
@@ -36,7 +39,7 @@ const NavBar = () => {
           Portfolio
         </Link>
       </ul>
-    </div>
+    </nav>
   );
 };
 
