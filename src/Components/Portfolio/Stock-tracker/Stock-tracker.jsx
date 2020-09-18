645031ac39202807fd04328b20d25594c64e7217
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-09-18 18:33:04 +0100

    some text changes

--- src/Components/Portfolio/Stock-tracker/Stock-tracker.jsx
+++ src/Components/Portfolio/Stock-tracker/Stock-tracker.jsx
@@ -11,8 +11,8 @@ const StockTracker = () => {
       </video>
       <div className="text-container">
         <h2 className="card-title">Personal Portfolio</h2>
-        <p>As an avid investor I decided to build a Web application using React.js (Hooks, Redux), Firebase and the IEX cloud API to track my investment portfolio.</p>
-        <p>Due to the sensitive nature of this project it is not hosted on the web. Source code is viewable on my github account.</p>
+        <p>As an avid investor I decided to build a Web application using React.js (Hooks, Redux, Redux-persist), Firebase and the IEX cloud API to track my investment portfolio.</p>
+        <p>Due to the sensitive nature of this project it is not hosted on the web. Source code is available on my github account.</p>
       </div>
       <div className="links" id='stocks'>
         <div className="link-item">
